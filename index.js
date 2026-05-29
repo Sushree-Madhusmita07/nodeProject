b8ffbca5e6042928b186b3dd0cf5f91c217e999d
Sus <madhusmitaj494@gmail.com>
2026-05-29 17:37:39 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-   res.write("Hello from Jenkins Auto Deployment 8");
+   res.write("Hello from Jenkins Auto Deployment 7");
 
    res.end();
 
